359020db57984afda343edb0b5971335374843de
nihilo-mike <mikyasmillion851@gmail.com>
2020-12-31 11:52:58 +0800

    bug fix

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -9,6 +9,3 @@ insert into account_type values (8,'loss');
 insert into account_type values (9,'gain');
 insert into sub_account_type values ( 1,'accounts payable' );
 insert into sub_account_type values ( 2,'day to day' );
-insert into debit_transaction values(1000,'food','2020-02-01T00:00:00Z',5,2);
-insert into credit_transaction values(1000,'food','2020-02-01T00:00:00Z',5,2);
-insert into credit_transaction values(1000,'book','2012-04-01T00:00:00Z',5,2);
