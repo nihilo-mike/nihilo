ddc004f1b0a3e782dff113546ecdfb4adfe94093
nihilo-mike <mikyasmillion851@gmail.com>
2021-02-02 11:45:59 +0800

    cors config

--- src/main/java/com/nihilo/nihilo/controller/DebitTransactionController.java
+++ src/main/java/com/nihilo/nihilo/controller/DebitTransactionController.java
@@ -11,6 +11,7 @@ import com.nihilo.nihilo.repository.DebitTransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = "*",maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class DebitTransactionController {
