6d5e50c101828b331712eaef356b8e83cc3009d1
nihilo-mike <mikyasmillion851@gmail.com>
2021-02-16 13:29:39 +0800

    commit on main

--- src/main/java/com/nihilo/nihilo/repository/SubAccountTypeRepository.java
+++ src/main/java/com/nihilo/nihilo/repository/SubAccountTypeRepository.java
@@ -1,12 +1,13 @@
 package com.nihilo.nihilo.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.nihilo.nihilo.model.SubAccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SubAccountTypeRepository extends JpaRepository<SubAccountType,Long> {
-   List<SubAccountType> findBySubId(Long subId);
+public interface SubAccountTypeRepository extends JpaRepository<SubAccountType, Long> {
+   Optional<SubAccountType> findById(Long subId);
    List<SubAccountType> findByParent(int parent);
   
 
