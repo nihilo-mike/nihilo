4a48a3e4209f221a4c1c12ebf4027966a4672da4
nihilo-mike <mikyasmillion851@gmail.com>
2021-02-16 12:22:28 +0800

    bug fixes

--- src/main/java/com/nihilo/nihilo/repository/CreditTransactionRepository.java
+++ src/main/java/com/nihilo/nihilo/repository/CreditTransactionRepository.java
@@ -3,14 +3,17 @@ package com.nihilo.nihilo.repository;
 import com.nihilo.nihilo.model.CreditTransaction;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.Instant;
 import java.util.List;
+
 
 public interface CreditTransactionRepository extends JpaRepository<CreditTransaction,Long> {
     List<CreditTransaction> findCreditTransactionByAccountTypeAndSubAccountType(Long id, Long subId );
     List<CreditTransaction>findAllByDate(Instant date);
     List<CreditTransaction>findAll();
-    List<CreditTransaction>findAllBySubAccountType(final Long subId);
+    @Query("SELECT * from CreditTransaction as creditTransaction WHERE creditTransaction.subAccountType.subId=:subId")
+    List<CreditTransaction>findAllBySubAccountType(Long subId);
     
   }
