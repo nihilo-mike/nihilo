39a382371e6bd63dcfdeac3902a45f84f1749419
nihilo-mike <mikyasmillion851@gmail.com>
2021-03-06 13:21:19 +0800

    commit on main

--- src/main/java/com/nihilo/nihilo/repository/TransactionRepository.java
+++ src/main/java/com/nihilo/nihilo/repository/TransactionRepository.java
@@ -1,18 +1,10 @@
 package com.nihilo.nihilo.repository;
-import java.util.List;
 
 import com.nihilo.nihilo.model.Transactions;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface TransactionRepository extends PagingAndSortingRepository<Transactions,Long> {
-  List<Transactions>findByTransId(Long transId,Pageable pageable);
-  List<Transactions>findAll(Pageable pageable);   
+  
 
-
-
-
-
-    
 }
