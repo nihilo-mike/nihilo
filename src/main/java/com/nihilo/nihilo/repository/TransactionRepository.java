b3076fb11bc9a33a6cfba362b8ddc2ce437e268e
nihilo-mike <mikyasmillion851@gmail.com>
2021-03-20 11:03:17 +0800

    commit on main

--- src/main/java/com/nihilo/nihilo/repository/TransactionRepository.java
+++ src/main/java/com/nihilo/nihilo/repository/TransactionRepository.java
@@ -5,16 +5,12 @@ import java.util.Optional;
 
 import com.nihilo.nihilo.model.Transactions;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 
 public interface TransactionRepository extends PagingAndSortingRepository<Transactions,Long> {
   
 
-@Query(value = "SELECT t from Transactions t  OUTER JOIN  t.creditTransactions c where c.remark LIKE CONCAT('%',UPPER(:remark),'%')"+
-"OR t OUTER JOIN t.debitTransactions d where d.remark LIKE CONCAT('%',UPPER(:remark),'%') ")
-Optional<Transactions>searchByRemark(@Param("remark") String remark);
+Optional<Transactions>findByCreditTransactions_RemarkOrDebitTransactions_Remark(String remark);
 
 
 
